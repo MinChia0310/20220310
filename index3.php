862e059d52613980adff6da58192e9bbf3455187
宋旻家 <100329489+MinChia0310@users.noreply.github.com>
2022-03-14 18:24:12 +0800

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+<meta name="keywords" content="墾丁, 三日遊, 旅遊, 海邊">
+<title>Kenting 三日遊</title>
+
+</head>
+
+<?php
+echo "<body bgcolor='#CAD8E6'>";
+
+?>
+
+
+<center><h1>墾丁三日遊</h1></center>
+
+<a name="#top">
+<b><h2>1.行程內容</h2></b>
+<hr color='#78ABDE' width="100%"/>
+<p><b><h4><span style="border:3px red dashed;">Day1: 墾丁國家公園</span></h4></b>墾丁國家公園之地形景觀優美，概估可分為東西兩部份，中間隔以狹長而南北延伸之恒春縱谷平原，西岸以臨海珊瑚礁斷崖地形為主，海崖遍佈裙狀珊瑚礁，北部大多為山區。在南北向延伸的斷層縱谷平原內，有一潭面廣闊的龍鑾潭，平原以東有隆起的珊瑚礁台地與石灰岩洞穴景觀。</p>
+<a href="https://www.ktnp.gov.tw/"><img src="/img/墾丁2.jpg" border="2" width="20%"></a><br/>
+
+<p><b><h4><span style="border:3px red dashed;">Day2: 愛琴海岸</span></h4></b>走進餐廳就能欣賞一望無際的藍色大海，
+觀海區還有繽紛彩繪牆、天空步道和浪漫造景，來愛琴海岸用餐午茶看海超有渡假的放鬆感，絕對是情侶約會和閨蜜美拍的好去處</p>
+<a href="https://www.facebook.com/%E6%84%9B%E7%90%B4%E6%B5%B7%E5%B2%B8%E6%B5%B7%E6%99%AF%E4%BC%91%E9%96%92%E6%B8%A1%E5%81%87%E5%9C%92%E5%8D%80-604331433292447/"><img src="/img/墾丁3.jpg" border="2" width="20%"></a><br/>
+
+<p><b><h4><span style="border:3px red dashed;">Day3: 邱家生魚片</span></h4></b>來到屏東墾丁、後壁湖遊玩不可錯過的必吃美食，推薦到邱家生魚片享用尚青的海鮮！</p>
+<a href="https://qiusashimi.com/"><img src="/img/墾丁4.jpg" border="2" width="20%"></a><br/>
+
+<b><h2>2.報名資料</h2></b>
+<hr color='#78ABDE' width="100%"/>
+<br/>姓名: <input type="text" name="name" >
+<br/>Email:<input type="email" name="Email" >
+<br/>電話: <input type="tel" name="Tel" >
+
+<br/>性別: Male<input type="radio" name="Male" >Female<input type="radio" name="Male" >
+<br/>食物偏好: 葷食<input type="checkbox" name="food" checked>素食<input type="checkbox" name="food" ><br/>
+
+
+<br/>T-shirt size:<br/>
+<select name="shirt" multiple>
+<option>S</option>
+<option>M</option>
+<option>L</option>
+<option>XL</option>
+<option>2L</option>
+</select>
+
+
+<br/>T-shirt顏色: <input type="text" name="color" >
+<br/>生日:<input type="date" name="Date" >
+
+<br/>Tickets:<input type="number" name="Number" >
+
+
+
+<br/><input type="submit">
+
+
+<a href="#top">回頁首</a><br/>
+
+<table border="1">
+
+
+<h4>T-shirt 尺寸對照表</h4>
+<tr><th> </th><th>S</th><th>M</th><th>L</th><th>XL</th></tr>
+<tr><td>胸寬</td><td>53</td><td>56</td><td>58</td><td>62</td></tr>
+<tr><td>身長</td><td>72</td><td>75</td><td>78</td><td>80</td></tr>
+</table>
+
+
+
+
+
+
+</body>
+
+
+</html>
